2fb518f2d9c376a6f88ae4ead4e98aabeeb2d8bc
Yannick Schutz <yannick.schutz@gmail.com>
2014-03-28 16:38:32 +0100

    Patch

--- lib/config/heroku/command/d2l.rb
+++ lib/config/heroku/command/d2l.rb
@@ -49,11 +49,36 @@ class Heroku::Command::D2L < Heroku::Command::Base
     puts JSON.pretty_generate(JSON.parse(res.body))
   end
 
+  # update [id]
+  #
+  # Update a measurment for dataclip
+  #
+  #
+  # -l, --librato  BASENAME  # specify librato base name
+  # -s, --source   LIBRATOSOURCE # the source
+  # -i, --interval RUNINTERVAL # the interval
+  # -d, --dataclip DATACLIP # the dataclip
+  #
   def update
-    puts 'update'
+    dataclip_reference = shift_argument
+    validate_arguments!
+    librato_base_name = options[:librato]
+    librato_source = options[:source]
+    run_interval = options[:interval]
+    uri = URI('http://libraclips.herokuapp.com/measurements')
+    req = Net::HTTP::Patch.new(uri.path)
+    req.basic_auth 'tools', 'team'
+    req['Content-Type'] = 'application/json'
+    body = {}
+    body[:dataclip_reference] = dataclip_reference if dataclip_reference
+    body[:librato_base_name] = librato_base_name if librato_base_name
+    body[:librato_source] = librato_source if librato_source
+    body[:run_interval] = run_interval if run_interval
+    req.body = body.to_json
+    res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+      http.request(req)
+    }
+    puts JSON.pretty_generate(JSON.parse(res.body))
   end
-
-  private
-
 end
 
